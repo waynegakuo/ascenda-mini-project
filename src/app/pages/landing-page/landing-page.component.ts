82072a02b0f6a167dab21b955caab26cb2198a85
waynegakuo <waynejotham@gmail.com>
2022-07-07 00:55:26 +0300

    feature(project): persist currency selection, set 'US' as default currency if no currency selection found

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import { Currency } from "../../models/currency.model";
+import {Currency} from "../../models/currency.model";
 import {HotelService} from "../../services/hotel/hotel.service";
 import {Hotel} from "../../models/hotel.model";
-import {BehaviorSubject, map, Observable, switchMap, take} from "rxjs";
-import {AbstractControl, FormBuilder, ValidationErrors, Validators} from "@angular/forms";
+import {BehaviorSubject, Observable} from "rxjs";
+import {FormBuilder, Validators} from "@angular/forms";
 import {PriceService} from "../../services/price/price.service";
 import {Price} from "../../models/price.model";
 
@@ -43,11 +43,11 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   filteredCurrency!: Currency[];
   currencySymbol!: string;
 
-  currencyValue!: string
+  currencyValue: string = JSON.parse(String(localStorage.getItem('selectedCurrency')))
 
   // Default Currency is USD
   currencyForm = this.fb.group({
-    currency: ['USD', Validators.required]
+    currency: [this.currencyValue, Validators.required]
   })
 
   // Default Currency Value
@@ -57,11 +57,32 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   constructor(private hotelService: HotelService, private fb: FormBuilder, private priceService: PriceService) { }
 
   ngOnInit(): void {
-    this.prices$ = this.priceService.getPrice(this.currencyValue$.value['currency']);
-    this._updateCurrencySymbol(this.currencyValue$.value['currency']);
-    this._updateCurrencyValue(this.currencyValue$.value['currency']);
+    this.storageChecker();
+    this.prices$ = this.priceService.getPrice(this.currencyValue);
+    this._updateCurrencySymbol(this.currencyValue);
+    this._updateCurrencyValue(this.currencyValue);
   }
 
+  /**
+   * Checks the local storage for previously selected currency
+   * sets the currency to 'US' if no currency selection found
+   * @return string
+   */
+  storageChecker(): string {
+    if(localStorage.getItem('selectedCurrency') === null){
+      localStorage.setItem('selectedCurrency', JSON.stringify('USD'));
+      this.currencyValue = JSON.parse(String(localStorage.getItem('selectedCurrency')));
+      this.currencyForm.controls.currency.setValue(this.currencyValue);
+    }
+    return this.currencyValue
+  }
+
+  /**
+   * Updates the currency selection via the dropdown selected changes
+   * triggers price change to render to view
+   * updates the currency symbols and currency value to be stored in local storage
+   * @param currencyValue
+   */
   onCurrencyOptionSelected(currencyValue: Event){
     this.currencyValue$.next({currency: (currencyValue.target as HTMLInputElement).value});
     this.prices$ = this.priceService.getPrice(this.currencyValue$.value['currency']);
@@ -69,6 +90,11 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     this._updateCurrencyValue(this.currencyValue$.value['currency']);
   }
 
+  /**
+   * Updates the currency symbol to be shown in the UI
+   * @param currencySelected
+   * @private
+   */
   private _updateCurrencySymbol(currencySelected: string | null): string {
     this.filteredCurrency = this.currencies
       .filter(selectedCurrency => currencySelected === selectedCurrency.value);
@@ -76,8 +102,14 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     return this.currencySymbol;
   }
 
-  private _updateCurrencyValue(currencyValueSelected: string | null) {
-    this.currencyValue = String(currencyValueSelected)
+  /**
+   * Updates the currency value and updates the localStorage currency value item
+   * @param currencySelected
+   * @private
+   */
+  private _updateCurrencyValue(currencySelected: string | null) {
+    localStorage.setItem('selectedCurrency', JSON.stringify(currencySelected));
+    this.currencyValue = JSON.parse(String(localStorage.getItem('selectedCurrency')))
     return this.currencyValue;
   }
 
